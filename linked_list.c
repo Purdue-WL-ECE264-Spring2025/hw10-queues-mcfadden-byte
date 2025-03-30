80a02417db2fb240874bd46ddaaed27f279999aa
texturelesshorse <texturelesshorse@gmail.com>
2025-03-30 18:50:03 -0400

    Zero

--- linked_list.c
+++ linked_list.c
@@ -2,15 +2,46 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) {
+	struct list_node * result = malloc(sizeof(struct list_node));
+	result -> value = value; result -> next = NULL;
+	return result;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+	struct list_node * node = new_node(value);
+	node -> next = list -> head;
+	list -> head = node;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+	while (list -> head -> next != NULL){
+		list -> head = list -> head -> next;
+	}
+	list -> head -> next = new_node(value);
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) {
+	size_t result = list -> head -> value;
+	struct list_node * temp = list -> head;
+	list -> head = list -> head -> next;
+	free(temp);
+	return result;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) {
+	size_t result;
+
+	while(list -> head -> next -> next != NULL){
+		list -> head = list -> head -> next;
+	}
+
+	result = list -> head -> next -> value;
+	free(list -> head -> next);
+	list -> head -> next = NULL;
+
+	return result;
+}
 
 void free_list(struct linked_list list) {}
 
--- queue.c
+++ queue.c
@@ -1,8 +1,57 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+	insert_at_tail(&(q -> data), serialize(state));
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) {
+	struct game_state result = deserialize((q -> data).head -> value);
 
-int number_of_moves(struct game_state start) { return 0; }
+	remove_from_head(&(q -> data));
+
+	return result;
+}
+
+int number_of_moves(struct game_state start) {
+	
+
+	/*struct game_state up = start;	
+	struct game_state down = start;
+	struct game_state left = start;
+	struct game_state right = start;
+
+	move_up(&up);
+	move_down(&down);
+	move_left(&left);
+	move_right(&right);*/
+
+	struct game_state udlr[4] = {start, start, start, start};
+	move_up(udlr);
+	move_down(udlr+1);
+	move_left(udlr+2);
+	move_right(udlr+3);
+
+	//check if any are correct
+	//if not, call self on each new one
+	int counter = 1;
+	int is_correct = 1;
+	for (int i = 0; i < 4; i++){
+		is_correct = 1;
+		for (int j = 0; j < 4; j++){
+			for (int k = 0; k < 4; k++){
+				if (udlr[i].tiles[j][k] != counter && counter != 16){
+					is_correct = 0;
+					break;
+				}
+				counter++;
+			}
+		}
+		if (is_correct != 0){
+			return udlr[i].num_steps;
+		}
+
+	}
+
+	return 0;
+}
