6ec1383087e97d9fb92b300f351aa405aaf9f7b6
texturelesshorse <texturelesshorse@gmail.com>
2025-03-31 09:51:19 -0400

    S1

--- linked_list.c
+++ linked_list.c
@@ -8,7 +8,7 @@ struct list_node *new_node(size_t value) {
 	return result;
 }
 
-void insert_at_head(struct linked_list *list, size_t value) {
+void insert_at_head(struct linked_list *list, size_t value){
 	struct list_node * node = new_node(value);
 	node -> next = list -> head;
 	list -> head = node;
@@ -28,25 +28,43 @@ void insert_at_tail(struct linked_list *list, size_t value) {
 
 size_t remove_from_head(struct linked_list *list) {
 	size_t result = list -> head -> value;
+	struct list_node * temp = list -> head;
 	list -> head = list -> head -> next;
+	free(temp);
 	return result;
 }
 
 size_t remove_from_tail(struct linked_list *list) {
 	size_t result;
+	if (list -> head -> next == NULL){
+		result = list -> head -> value;
+		free(list -> head);
+		list -> head = NULL;
+	}else{
+		struct list_node * last = list -> head;
+		while(last -> next -> next != NULL){
+			last = last -> next;
+		}
 
-	while(list -> head -> next -> next != NULL){
-		list -> head = list -> head -> next;
+		result = last -> next -> value;
+		free(last -> next);
+		last -> next = NULL;
 	}
-
-	result = list -> head -> next -> value;
-	free(list -> head -> next);
-	list -> head -> next = NULL;
 
 	return result;
 }
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) {
+	if (list.head != NULL){
+		struct list_node * temp = list.head;
+		while (list.head -> next != NULL){
+			list.head = list.head -> next;
+			free(temp);
+			temp = list.head;
+		}
+		free(temp);
+	}
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,17 +1,34 @@
 #include "queue.h"
 #include "tile_game.h"
 
+//void print_q(struct queue *);
+
 void enqueue(struct queue *q, struct game_state state) {
-	insert_at_tail(&(q -> data), serialize(state));
+	insert_at_head(&(q -> data), serialize(state));
 }
 
 struct game_state dequeue(struct queue *q) {
-	struct game_state result = deserialize((q -> data).head -> value);
-
-	remove_from_head(&(q -> data));
+	struct game_state result = deserialize(remove_from_tail(&(q -> data)));
 
 	return result;
 }
+
+/*void print_q(struct queue * q){
+	struct list_node * curr = (q -> data).head;
+	printf("QUE START:\n");
+	do{
+		struct game_state state = deserialize(curr -> value);
+		for (int i = 0; i < 4; i++){
+			for (int j = 0; j < 4; j++){
+				printf("%d ", state.tiles[i][j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+		curr = curr -> next;
+	}while (curr != NULL);
+}*/
+
 
 int number_of_moves(struct game_state start) {
 	
@@ -23,28 +40,36 @@ int number_of_moves(struct game_state start) {
 	//Set up the first thing in queue, which is the input grid
 	struct queue que;
 	struct queue * q = &que;
-	struct list_node first; first.value = serialize(start); first.next = NULL;
-	(q -> data).head = &first;
+	(q -> data).head = NULL;
+	//struct list_node * first = malloc(sizeof(struct list_node));
+	//first -> value = serialize(start); first -> next = NULL;
+	//(q -> data).head = first;
+	enqueue(q, start);
 	int found = 0;
 	int num_steps = 0;
+	int que_length = 1;
 
 	
 	while (found == 0){
 		struct game_state temp = dequeue(q);
+		que_length--;
 		//printf("NUM STEPS: %d\n", temp.num_steps);
+		//printf("QUE LENGTH: %d\n", que_length);
 
 		//Check if first thing in queue is correct
 		int equal = 1;
 		int counter = 1;
 		for (int i = 0; i < 4; i++){
 			for (int j = 0; j < 4; j++){
+				//printf("%02d ", temp.tiles[i][j]);
 				if (temp.tiles[i][j] != counter && counter != 16){
 					equal = 0;
-					break;
 				}
 				counter++;
 			}
+			//printf("\n");
 		}
+		//printf("\n");
 
 		//If first thing in queue is not correct, create four copies, one for each move, and add them to the back of queue
 		if (equal == 0){
@@ -53,10 +78,16 @@ int number_of_moves(struct game_state start) {
 			move_down(udlr+1);
 			move_left(udlr+2);
 			move_right(udlr+3);
-
+			
 			for (int i = 0; i < 4; i++){
-				enqueue(q, udlr[i]);
+				if (udlr[i].num_steps == temp.num_steps + 1){
+					enqueue(q, udlr[i]);
+				}
+				/*if (udlr[i].num_steps == 1){
+					printf("NUM_STEPS = 1 HAS BEEN ADDED TO QUE! MOVE WAS: %d\n", i);
+				}*/
 			}
+			que_length += 4;
 		}else{
 			//If first thing in queue is correct, record the number of steps
 			found = 1;
@@ -64,6 +95,8 @@ int number_of_moves(struct game_state start) {
 		}
 
 	}
+
+	free_list(q -> data);
 
 	return num_steps;
 }
