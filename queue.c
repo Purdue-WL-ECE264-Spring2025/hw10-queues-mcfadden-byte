2bce3b345dc68a3a901da5f0122807296e45343e
texturelesshorse <texturelesshorse@gmail.com>
2025-04-01 12:31:32 -0400

    Clean

--- queue.c
+++ queue.c
@@ -19,14 +19,15 @@ int number_of_moves(struct game_state start) {
 	//If not correct, create four temp copies, execute different move on each, place each in back of queue
 	//Advance to next item in queue
 
-	//Set up the first thing in queue, which is the input grid
+	//Set up the first thing in the test queue, which is the input grid
 	struct queue que;
 	struct queue * q = &que;
 	(q -> data).head = NULL;
 	enqueue(q, start);
 	int found = 0;
 	int num_steps = 0;
-	//int que_length = 1;
+
+	//initialize the goal as a serlialized game state, with num_steps = 0
 	int goal[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
 	struct game_state tile_goal; tile_goal.num_steps = 0;
 	for (int i = 0; i < 4; i++){
@@ -36,6 +37,9 @@ int number_of_moves(struct game_state start) {
 	}
 	size_t tg_ser = serialize(tile_goal);
 
+	//Make a queue of all game states we've seen before (always set num_steps to zero
+	//before adding to this queue, since we only care about the tile arrangement).
+	//We treat this like a list, it's just a queue for convenience in programming.
 	struct queue seen_before;
 	struct queue * sb = &seen_before;
 	(sb -> data).head = NULL;
@@ -47,10 +51,6 @@ int number_of_moves(struct game_state start) {
 		struct game_state temp = dequeue(q);
 		struct game_state tile_copy = temp; tile_copy.num_steps = 0;
 
-		//que_length--;
-		//printf("NUM STEPS: %d\n", temp.num_steps);
-		//printf("QUE LENGTH: %d\n", que_length);
-
 		//Check if first thing in queue is correct
 		//If first thing in queue is not correct, create four copies, one for each move, and add them to the back of queue
 		if (serialize(tile_copy) != tg_ser){
@@ -61,25 +61,27 @@ int number_of_moves(struct game_state start) {
 			move_right(udlr+3);
 
 			for (int i = 0; i < 4; i++){
+				
+				//Check if already encountered
+				
 				struct game_state seen_copy = udlr[i];
 				seen_copy.num_steps = 0;
-				//Check if already encountered
 				int copy = 0;
-				//For everything in the queue...
 				struct list_node * curr = (sb -> data).head;
 				size_t sc_ser = serialize(seen_copy);
 
+				//For everything in the queue...
 				while (curr -> next != NULL && copy == 0){
-					//Check if seen before
+					//Check if seen before (serialized with num_steps = 0 equal to serialized of any game state seen before)
 					if (sc_ser == curr -> value){
 						copy = 1;
 					}
 					curr = curr -> next;
 				}
 
+				//If it's not something we've seen before, add it to the queue, and add it to the list of things we've seen.
 				if (udlr[i].num_steps == temp.num_steps + 1 && copy == 0){
 					enqueue(q, udlr[i]);
-					//que_length++;
 					enqueue(sb, seen_copy);
 				}
 			}
