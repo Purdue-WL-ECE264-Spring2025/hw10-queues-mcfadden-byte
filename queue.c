9e523f6988c2bbf0a4d5633ccbcde0cd3dc5f671
texturelesshorse <texturelesshorse@gmail.com>
2025-04-01 11:32:32 -0400

    Faster

--- queue.c
+++ queue.c
@@ -26,10 +26,15 @@ int number_of_moves(struct game_state start) {
 	enqueue(q, start);
 	int found = 0;
 	int num_steps = 0;
-	int que_length = 1;
-	//size_t prev[500000] = {0};
-	//int index = 0;
+	//int que_length = 1;
 	int goal[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
+	struct game_state tile_goal; tile_goal.num_steps = 0;
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 4; j++){
+			tile_goal.tiles[i][j] = goal[i][j];
+		}
+	}
+	size_t tg_ser = serialize(tile_goal);
 
 	struct queue seen_before;
 	struct queue * sb = &seen_before;
@@ -40,47 +45,21 @@ int number_of_moves(struct game_state start) {
 	
 	while (found == 0){
 		struct game_state temp = dequeue(q);
-		//prev[index] = serialize(temp);
-		//index++;
-		que_length--;
+		struct game_state tile_copy = temp; tile_copy.num_steps = 0;
+
+		//que_length--;
 		//printf("NUM STEPS: %d\n", temp.num_steps);
 		//printf("QUE LENGTH: %d\n", que_length);
 
 		//Check if first thing in queue is correct
-		int equal = 1;
-		for (int i = 0; i < 4 && equal == 1; i++){
-			for (int j = 0; j < 4 && equal == 1; j++){
-				//printf("%02d ", temp.tiles[i][j]);
-				if (temp.tiles[i][j] != goal[i][j]){
-					equal = 0;
-				}
-			}
-			//printf("\n");
-		}
-		//printf("\n");
-
 		//If first thing in queue is not correct, create four copies, one for each move, and add them to the back of queue
-		if (equal == 0){
+		if (serialize(tile_copy) != tg_ser){
 			struct game_state udlr[4] = {temp, temp, temp, temp};
-			/*if (temp.empty_row != 3){
-				move_down(udlr+1);
-			}
-			if (temp.empty_row != 0){
-				move_up(udlr);
-			}
-			if (temp.empty_col != 3){
-				move_right(udlr+3);
-			}
-			if (temp.empty_col != 0){
-				move_left(udlr+2);
-			}*/
-			
 			move_up(udlr);
 			move_down(udlr+1);
 			move_left(udlr+2);
 			move_right(udlr+3);
 
-			//printf("NUM ENQUEUED: ");
 			for (int i = 0; i < 4; i++){
 				struct game_state seen_copy = udlr[i];
 				seen_copy.num_steps = 0;
@@ -89,39 +68,23 @@ int number_of_moves(struct game_state start) {
 				if (udlr[i].num_steps == temp.num_steps + 1){
 					copy = 0;
 				}
+				
 				//For everything in the queue...
-				//if ((q->data).head != NULL){
-					struct list_node * curr = (sb -> data).head;
-					while (curr -> next != NULL && copy == 0){
-						/*
-						//Check if equal to compare
-						int copy_of_compare = 1;
-						struct game_state compare = deserialize(curr -> value);
-						for (int j = 0; j < 4 && copy_of_compare == 1; j++){
-							for (int k = 0; k < 4 && copy_of_compare == 1; k++){
-								if (udlr[i].tiles[j][k] != compare.tiles[j][k]){
-									copy_of_compare = 0;
-								}
-							}
-						}
-						if (copy_of_compare == 1){
-							copy = 1;
-						}*/
-						if (serialize(seen_copy) == curr -> value){
-							copy = 1;
-						}
-
-						curr = curr -> next;
+				struct list_node * curr = (sb -> data).head;
+				while (curr -> next != NULL && copy == 0){
+					//Check if seen before
+					if (serialize(seen_copy) == curr -> value){
+						copy = 1;
 					}
-				//}
+					curr = curr -> next;
+				}
 
 				if (udlr[i].num_steps == temp.num_steps + 1 && copy == 0){
 					enqueue(q, udlr[i]);
-					que_length++;
+					//que_length++;
 					enqueue(sb, seen_copy);
 				}
 			}
-			//printf("\n");
 		}else{
 			//If first thing in queue is correct, record the number of steps
 			found = 1;
